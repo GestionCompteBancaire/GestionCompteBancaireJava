d47e2e67d0a2c1ddacf4caa10b55af00f52f4f42
Kaoutar2001 <85807224+Kaoutar2001@users.noreply.github.com>
2021-06-12 22:46:00 +0100

    Add files via upload

---
+++ src/Vue/Proj.java
@@ -0,0 +1,233 @@
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextPane;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JTable;
+import java.awt.Color;
+import javax.swing.ListSelectionModel;
+import javax.swing.border.BevelBorder;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.border.TitledBorder;
+import javax.swing.border.EtchedBorder;
+import javax.swing.JTextField;
+import javax.swing.border.MatteBorder;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class Proj extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
+	private JTextField textField_5;
+	private JTextField textField_6;
+	private JTextField textField_7;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Proj frame = new Proj();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Proj() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 808, 522);
+		contentPane = new JPanel();
+		contentPane.setBackground(Color.WHITE);
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Gestion de donn\u00E9es");
+		lblNewLabel.setForeground(new Color(30, 144, 255));
+		lblNewLabel.setFont(new Font("Forte", Font.BOLD | Font.ITALIC, 24));
+		lblNewLabel.setBounds(258, 11, 270, 31);
+		contentPane.add(lblNewLabel);
+		
+		JPanel panel = new JPanel();
+		panel.setBorder(new TitledBorder(null, "Information", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panel.setBackground(Color.WHITE);
+		panel.setBounds(10, 53, 328, 301);
+		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel_1 = new JLabel("Nom :");
+		lblNewLabel_1.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, 14));
+		lblNewLabel_1.setBounds(17, 20, 59, 20);
+		panel.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_1_1 = new JLabel("Pr\u00E9nom");
+		lblNewLabel_1_1.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, 14));
+		lblNewLabel_1_1.setBounds(17, 63, 59, 20);
+		panel.add(lblNewLabel_1_1);
+		
+		JLabel lblNewLabel_1_2 = new JLabel("CIN :");
+		lblNewLabel_1_2.setFont(new Font("Times New Roman", Font.BOLD, 14));
+		lblNewLabel_1_2.setBounds(17, 100, 59, 20);
+		panel.add(lblNewLabel_1_2);
+		
+		JLabel lblNewLabel_1_3 = new JLabel("N\u00B0 de t\u00E9l\u00E9phone :");
+		lblNewLabel_1_3.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, 12));
+		lblNewLabel_1_3.setBounds(17, 142, 91, 20);
+		panel.add(lblNewLabel_1_3);
+		
+		JLabel lblNewLabel_1_4 = new JLabel("Solde :");
+		lblNewLabel_1_4.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, 13));
+		lblNewLabel_1_4.setBounds(17, 184, 59, 20);
+		panel.add(lblNewLabel_1_4);
+		
+		JLabel lblNewLabel_1_4_1 = new JLabel("Date de naissance :");
+		lblNewLabel_1_4_1.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, 12));
+		lblNewLabel_1_4_1.setBounds(10, 225, 98, 20);
+		panel.add(lblNewLabel_1_4_1);
+		
+		JLabel lblNewLabel_1_4_2 = new JLabel("Adresse");
+		lblNewLabel_1_4_2.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, 13));
+		lblNewLabel_1_4_2.setBounds(10, 261, 59, 20);
+		panel.add(lblNewLabel_1_4_2);
+		
+		textField = new JTextField();
+		textField.setBounds(112, 21, 197, 20);
+		panel.add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(112, 64, 197, 20);
+		panel.add(textField_1);
+		
+		textField_2 = new JTextField();
+		textField_2.setColumns(10);
+		textField_2.setBounds(112, 101, 197, 20);
+		panel.add(textField_2);
+		
+		textField_3 = new JTextField();
+		textField_3.setColumns(10);
+		textField_3.setBounds(112, 142, 197, 20);
+		panel.add(textField_3);
+		
+		textField_4 = new JTextField();
+		textField_4.setColumns(10);
+		textField_4.setBounds(112, 184, 197, 20);
+		panel.add(textField_4);
+		
+		textField_5 = new JTextField();
+		textField_5.setColumns(10);
+		textField_5.setBounds(112, 225, 197, 20);
+		panel.add(textField_5);
+		
+		textField_6 = new JTextField();
+		textField_6.setColumns(10);
+		textField_6.setBounds(112, 261, 197, 20);
+		panel.add(textField_6);
+		
+		JButton btnNewButton = new JButton("Ajouter");
+		btnNewButton.setBackground(new Color(255, 215, 0));
+		btnNewButton.setFont(new Font("Times New Roman", Font.BOLD, 15));
+		btnNewButton.setBounds(10, 365, 89, 31);
+		contentPane.add(btnNewButton);
+		
+		JButton btnCrditer = new JButton("Cr\u00E9diter ");
+		btnCrditer.setBackground(new Color(255, 215, 0));
+		btnCrditer.setFont(new Font("Times New Roman", Font.BOLD, 15));
+		btnCrditer.setBounds(122, 365, 98, 31);
+		contentPane.add(btnCrditer);
+		
+		JButton btnDbiter = new JButton("D\u00E9biter");
+		btnDbiter.setBackground(new Color(255, 215, 0));
+		btnDbiter.setFont(new Font("Times New Roman", Font.BOLD, 15));
+		btnDbiter.setBounds(249, 365, 89, 31);
+		contentPane.add(btnDbiter);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBorder(new TitledBorder(null, "Liste des clients", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panel_1.setBackground(Color.WHITE);
+		panel_1.setBounds(348, 53, 434, 256);
+		contentPane.add(panel_1);
+		panel_1.setLayout(null);
+		
+		JButton btnActualiser = new JButton("Actualiser");
+		btnActualiser.setBackground(new Color(255, 215, 0));
+		btnActualiser.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnActualiser.setFont(new Font("Times New Roman", Font.BOLD, 15));
+		btnActualiser.setBounds(411, 336, 107, 35);
+		contentPane.add(btnActualiser);
+		
+		JButton btnSupprimer = new JButton("Supprimer");
+		btnSupprimer.setBackground(new Color(255, 215, 0));
+		btnSupprimer.setFont(new Font("Times New Roman", Font.BOLD, 15));
+		btnSupprimer.setBounds(600, 336, 107, 35);
+		contentPane.add(btnSupprimer);
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.setBackground(Color.WHITE);
+		panel_2.setBorder(new TitledBorder(new EtchedBorder(EtchedBorder.LOWERED, new Color(255, 255, 255), new Color(160, 160, 160)), "Recherche", TitledBorder.LEADING, TitledBorder.TOP, null, Color.DARK_GRAY));
+		panel_2.setToolTipText("hhhh");
+		panel_2.setBounds(10, 413, 328, 58);
+		contentPane.add(panel_2);
+		panel_2.setLayout(null);
+		
+		
+		JLabel lblNewLabel_1_3_1 = new JLabel("N\u00B0 de compte :");
+		lblNewLabel_1_3_1.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, 14));
+		lblNewLabel_1_3_1.setBounds(10, 27, 91, 20);
+		panel_2.add(lblNewLabel_1_3_1);
+		
+		textField_7 = new JTextField();
+		textField_7.setColumns(10);
+		textField_7.setBounds(111, 28, 197, 20);
+		panel_2.add(textField_7);
+		
+		
+		String[] columnNames = {"First Name",
+                "Last Name",
+                "Sport",
+                "# of Years",
+                "Vegetarian"};
+		Object[][] data = {
+			    {"Kathy", "Smith",
+			     "Snowboarding", new Integer(5), new Boolean(false)},
+			    {"John", "Doe",
+			     "Rowing", new Integer(3), new Boolean(true)},
+			    {"Sue", "Black",
+			     "Knitting", new Integer(2), new Boolean(false)},
+			    {"Jane", "White",
+			     "Speed reading", new Integer(20), new Boolean(true)},
+			    {"Joe", "Brown",
+			     "Pool", new Integer(10), new Boolean(false)}
+			};
+		
+		JTable table = new JTable(data, columnNames);
+		
+		
+		
+	}
+}
